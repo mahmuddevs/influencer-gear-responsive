f0c6f626192dd2070dfa04cda57e8dbd1f382e17
Mahmud Rashid <srabon5704@gmail.com>
2024-08-13 00:14:42 +0600

    added banner section and made it responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase }) {
+      addBase(
+        {
+          'h1': { color: 'rgb(58, 58, 58)', fontWeight: 700 },
+          'p': { fontSize: '1rem', color: 'rgb(58, 58, 58)', fontWeight: 600 },
+          '.hr-border': { background: 'linear-gradient(135deg, rgba(192, 204, 218, 0.1) 0%, rgba(192, 204, 218, 0.6) 50.378%, rgba(192, 204, 218, 0.1) 100%)' },
+        }
+      )
+    }
+  ],
 }
 
