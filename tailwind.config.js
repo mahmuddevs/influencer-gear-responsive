faedf4095a1b5b3c5aad2c007376e7a07a5954d5
Mahmud Rashid <srabon5704@gmail.com>
2024-08-13 21:22:45 +0600

    added the product card grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,13 @@ module.exports = {
       addBase(
         {
           'h1': { color: 'rgb(58, 58, 58)', fontWeight: 700 },
+          'h2': { color: 'rgb(58, 58, 58)', fontWeight: 700 },
+          'h3': {
+            color: 'rgb(24, 25, 31)', fontSize: '1.75rem', fontWeight: 500
+          },
           'p': { fontSize: '1rem', color: 'rgb(58, 58, 58)', fontWeight: 600 },
           '.hr-border': { background: 'linear-gradient(135deg, rgba(192, 204, 218, 0.1) 0%, rgba(192, 204, 218, 0.6) 50.378%, rgba(192, 204, 218, 0.1) 100%)' },
+          '.card-shadow': { boxShadow: '0px 0px 40px 0px rgba(0, 0, 0, 0.15)' }
         }
       )
     }
